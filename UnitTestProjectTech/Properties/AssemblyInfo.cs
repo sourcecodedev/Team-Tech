e14c371bfb053a3b0fd9464e15b6c8ec85fde47b
Yhimy Wilberto Feria Casariego <yferia@sapia.com.pe>
2019-07-11 22:49:25 -0500

    Tech Final

---
+++ UnitTestProjectTech/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProjectTech")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProjectTech")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a6c08be3-eb71-43ea-8c73-8347e904d92c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProjectTech/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProjectTech
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestServiceCliente_Empresa()
+        {
+            ServiceClient.ServiceClientClient client = new ServiceClient.ServiceClientClient();
+            var data = client.ConsultDatosCompany("20100130204");
+
+            Assert.Equals("BBVA", data.nombre_comercial);
+        }
+
+        [TestMethod]
+        public void TestServiceCliente_Persona()
+        {
+            ServiceClient.ServiceClientClient client = new ServiceClient.ServiceClientClient();
+            var data = client.ConsultDatosPersonal("42595200");
+
+            Assert.Equals("SILVERA ABAD,ANDRES AVELINO", data.nombres);
+        }
+ 
+
+        [TestMethod]
+        public void TestServiceUsuario_Login_GetdataUsers()
+        {
+            ServiceUsuario.ServiceUsuarioClient usuario = new ServiceUsuario.ServiceUsuarioClient();
+            var data_usuario = usuario.GetDatabyUserTech("yferia");
+
+            Assert.Equals("Administrator", data_usuario.RolNombre);
+        }
+
+    }
+}
